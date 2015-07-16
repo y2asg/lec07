80bd80697158a18d3c4a62090d69c966ee582f6e
y_yana <y_yana@yanayukiko-no-MacBook-Air.local>
2015-07-16 23:13:22 +0900

    all files

---
+++ convert.php
@@ -0,0 +1,13 @@
+<html>	
+  <body>
+    <?php
+       $msg = htmlspecialchars($_GET["message"]);
+       
+       $len = mb_strlen($msg);
+       for($i = 0; $i < $len; ++$i){
+	$asc = ord($msg[$i]);
+	echo chr($asc + $i);
+	}
+     ?>
+  </body>
+</html>	
